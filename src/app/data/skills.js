ba5f2a1dac83fa340ea376ed5a3373a8586270bc
James Salcedo <jamezzzsalcedo17@gmail.com>
2026-02-13 01:03:21 -0600

    Complete skills file

---
+++ src/app/data/skills.js
@@ -0,0 +1,18 @@
+export const skills = [
+    {
+        group: "Frontend",
+        items: ["JavaScript", "React", "Next.js", "Tailwind", "HTML", "CSS"],
+    },
+    {
+        group: "Backend",
+        items: ["Ruby", "Ruby on Rails", "REST APIs"],
+    },
+    {
+        group: "Database",
+        items: ["PostgreSQL"]
+    }
+    {
+        group: "Tools",
+        items: ["Git/Github", "Stripe", "Postman", "Vercel", "Render"]
+    }
+]
