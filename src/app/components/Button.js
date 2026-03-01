c233ea8e23c6ad7c1f1dbbe1afe2ad3247eecbe9
James Salcedo <jamezzzsalcedo17@gmail.com>
2026-02-28 23:29:17 -0600

    Adjust button color

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,7 +1,7 @@
 "use client";
 
 export default function Button({ children, type = "button", className = "", ...props }) {
-    const baseStyles = "rounded-md bg-slate-700 px-4 py-2 text-sm text-white hover:bg-neutral-800 duration-300 delay-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-neutral-900";
+    const baseStyles = "rounded-md bg-slate-800 px-4 py-2 text-sm text-white hover:bg-neutral-800 duration-300 delay-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-neutral-900";
     
     return (
         <button
